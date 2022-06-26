50c838c1b6abecba37ae4b2a30afb00e69e744f7
daltynov <DAltynov@beeline.kz>
2022-06-26 10:42:17 +0600

    create gateway

---
+++ src/test/java/dar/academy/backend/postapigateway/PostApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package dar.academy.backend.postapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostApiGatewayApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
